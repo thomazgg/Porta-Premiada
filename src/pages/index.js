3d78903f110356cfa405c70eeb644f6ffd7eb72b
Thomaz <79727201+thomazgg@users.noreply.github.com>
2024-04-24 16:59:30 -0300

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Porta from "../../components/Porta";
+
+export default function Home() {
+  return (
+    <div style={{display: "flex"}}>
+      <Porta selecionada/>
+      <Porta />
+    </div>
+  );
+}
